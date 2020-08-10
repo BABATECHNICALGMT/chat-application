e79c08e2654d517a18f20f2cef472602fd90349f
sikendar kumar <www.akarnaha@gmail.com>
2020-08-09 21:15:11 -0700

    this is chat application by baba technical my frist commit for web

---
+++ clind.js
@@ -0,0 +1,41 @@
+const socket = io('http://localhost:8000');
+
+const form = document.getElementById('send_container');
+const messageInput = document.getElementById('messageInp');
+const messageContainer = document.querySelector(".chat_div");
+var audio = new Audio('pristine.mp3');
+
+const append = (message, position)=>{
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
+    if(position == 'left'){
+        audio.play();
+    }
+}
+
+form.addEventListener('submit', (e)=>{
+     e.preventDefault();
+     const message = messageInput.value;
+     append(`you : ${message}`,'right');
+     socket.emit('send', message);
+     messageInput.value = '';
+})
+
+const name = prompt('enter your name join :)');
+socket.emit('new-user-joined', name);
+
+socket.on('user-joined', name=>{
+   append(`${name} joined the chat`, 'right');
+});
+
+socket.on('receive', data=>{
+    append(`${data.name}: ${data.message}`,'left')
+ });
+
+socket.on('left', name=>{
+    append(`${name} : disconnect the chat`,'left')
+ })
+
